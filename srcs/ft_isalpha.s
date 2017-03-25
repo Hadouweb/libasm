048b71602975cbe4bc18518951a6e427bfce5de2
Nicolas LE BRETON <nle-bret@e1r9p4.42.fr>
2017-03-25 10:02:52 +0100

    OK

--- srcs/ft_isalpha.s
+++ srcs/ft_isalpha.s
@@ -1,31 +1,25 @@
-; Ptototype : int ft_isalpha(int c)
-;
-
 section .text
 	global _ft_isalpha
 
 _ft_isalpha:
-	push rbp			;
-	mov rbp, rsp		;
+	push	rbp
+	mov		rbp, rsp	; Sauvegarde de la pile
+	mov		rax, 0		; int ret = 0
+	
+	cmp		rdi, 'A'	; Si < 'A'
+	jl		end			; return 0
+	cmp		rdi, 'Z'	; Si <= 'Z' et >= 'A'
+	jle		istrue		; return 1
+	
+	cmp		rdi, 'a'	; Si < 'a'
+	jl		end			; return 0
+	cmp		rdi, 'z'	; Si <= 'z' et >= 'a'
+	jg		end			; return 0
 
-	cmp rdi, 'A'		;
-	jl pas_lettre		; if (c < 'A') goto pas_lettre;
-	cmp rdi, 'Z'		;
-	jle est_lettre		; if (c <= 'Z') goto est_lettre;
+istrue:
+	mov rax, 1			; registre pour la valeur de retour
+	jmp end				; FIN
 
-	cmp rdi, 'a'		;
-	jl pas_lettre		; if (c < 'a') goto pas_lettre;
-	cmp rdi, 'z'		;
-	jg pas_lettre		; if (c <= 'z') goto pas_lettre;
-
-est_lettre:
-	mov rax, 1
-	jmp fin
-
-pas_lettre:
-	mov rax, 0
-	jmp fin
-
-fin:
+end:
 	leave
 	ret
